41c82cac9021535352d05367aabd0d44e8b23be5
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-02-08 15:09:31 -0800

    write setup fuction element

--- app.cpp
+++ app.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime> 
 using namespace std;
+
 bool gameOver;
 const int width{20};
 const int height{20};
 int x, y, fruitX, fruitY, score;
+enum eDirection{STOP = 0, RIGHT, LEFT, UP, DOWN};
+eDirection dir;
+
 void setup(){
     gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
 }
 void draw(){
 
